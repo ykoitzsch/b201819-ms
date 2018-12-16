62e2459bb0fa0ed950232ac97fd6c6c2bb0a62aa
ykoitzsch <y.koitzsch@gmx.at>
2018-12-16 22:07:22 +0100

    product ratings now possible but only once per product

--- shop/src/main/webapp/app/entities/inventory/product/product-detail.component.ts
+++ shop/src/main/webapp/app/entities/inventory/product/product-detail.component.ts
@@ -6,6 +6,7 @@ import { AccountService } from '../../../core/auth/account.service';
 import { ProductOrder } from '../../../shared/model/orders/product-order.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { IProduct } from 'app/shared/model/inventory/product.model';
 import { BasketService } from '../../orders/basket';
@@ -21,6 +22,10 @@ export class ProductDetailComponent implements OnInit {
     account: Account;
     basket: Basket;
     ratings: Rating[];
+    currentRating: number;
+    customerDesc: String;
+    customerPoints: number;
+    customerRatedAlready: boolean;
 
     constructor(
         private activatedRoute: ActivatedRoute,
@@ -34,6 +39,16 @@ export class ProductDetailComponent implements OnInit {
         this.activatedRoute.data.subscribe(({ product }) => {
             this.product = product;
             this.amount = 1;
+            this.customerDesc = '';
+            this.customerPoints = 1;
+            this.currentRating = 0;
+            this.customerRatedAlready = false;
+        });
+
+        this.accountService.get().subscribe(response => {
+            if (response.status === 200) {
+                this.account = response.body;
+            }
         });
         this.loadRatings();
     }
@@ -43,32 +58,74 @@ export class ProductDetailComponent implements OnInit {
     }
 
     intoBasket(amount, product) {
-        this.accountService.get().subscribe(response => {
-            if (response.status === 200) {
-                this.account = response.body;
-                this.basketService.find(+this.account.id).subscribe(
-                    (res: HttpResponse<Basket>) => {
-                        this.basket = res.body;
-                        if (this.basket.productOrders === null) {
-                            this.basket.productOrders = [];
-                        }
-                        this.basket.productOrders.push(new ProductOrder(null, amount, +this.account.id, product.id, null, null));
-                        this.basketService.update(this.basket).subscribe((r: HttpResponse<Basket>) => {
-                            this.jhiAlertService.success(amount + 'x ' + product.name + ' has successfully been added to your basket!');
-                        });
-                    },
-                    (res: HttpErrorResponse) => {
-                        this.jhiAlertService.error(res.status + ': No basket with id ' + this.account.id + ' found');
-                    }
-                );
+        this.basketService.find(+this.account.id).subscribe(
+            (res: HttpResponse<Basket>) => {
+                this.basket = res.body;
+                if (this.basket.productOrders === null) {
+                    this.basket.productOrders = [];
+                }
+                this.basket.productOrders.push(new ProductOrder(null, amount, +this.account.id, product.id, null, null));
+                this.basketService.update(this.basket).subscribe((r: HttpResponse<Basket>) => {
+                    this.jhiAlertService.success(amount + 'x ' + product.name + ' has successfully been added to your basket!');
+                });
+            },
+            (res: HttpErrorResponse) => {
+                this.jhiAlertService.error(res.status + ': No basket with id ' + this.account.id + ' found');
             }
-        });
+        );
+    }
+
+    calculateRating() {
+        for (const r of this.ratings) {
+            this.currentRating = this.currentRating + r.points;
+        }
+        this.currentRating = this.currentRating / this.ratings.length;
+    }
+
+    rate(points, desc) {
+        this.ratingService.create(new Rating(null, points, this.product.id, +this.account.id, desc)).subscribe(
+            (res: HttpResponse<Rating>) => {
+                if (!this.ratingExists()) {
+                    this.ratings.push(res.body);
+                    this.ratingExists();
+                } else {
+                    this.jhiAlertService.error('You have already rated this product');
+                }
+            },
+            (res: HttpErrorResponse) => {
+                this.jhiAlertService.error('Rating could not been saved');
+            }
+        );
+    }
+    ratingExists() {
+        for (const r of this.ratings) {
+            if (r.customerId === +this.account.id) {
+                this.customerRatedAlready = true;
+                return true;
+            }
+        }
+        return false;
     }
 
     loadRatings() {
+        this.ratings = [];
+        let i = [];
         this.ratingService.query().subscribe(
             (res: HttpResponse<Rating[]>) => {
                 this.ratings = res.body;
+                for (const r of this.ratings) {
+                    if (r.productId !== this.product.id) {
+                        console.log('index -> ' + this.ratings.indexOf(r));
+                        i.push(this.ratings.indexOf(r));
+                        // this.ratings.splice(this.ratings.indexOf(r), 1);
+                    }
+                }
+                this.calculateRating();
+                for (const index of i) {
+                    console.log('remove -> ' + index);
+                    this.ratings.splice(index, 1);
+                }
+                this.ratingExists();
             },
             (res: HttpErrorResponse) => {
                 this.jhiAlertService.error('Rating Service is at the moment not available');
